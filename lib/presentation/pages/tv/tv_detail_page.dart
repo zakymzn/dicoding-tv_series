3319d97773a426f01d14d7b93a0faf9bb4f65df7
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-10-23 12:57:00 +0700

    Melengkapi widget-widget di tv detail page

--- lib/presentation/pages/tv/tv_detail_page.dart
+++ lib/presentation/pages/tv/tv_detail_page.dart
@@ -3,7 +3,6 @@ import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/tv/tv.dart';
 import 'package:ditonton/domain/entities/tv/tv_detail.dart';
-import 'package:ditonton/presentation/pages/movie/movie_detail_page.dart';
 import 'package:ditonton/presentation/provider/tv/tv_detail_notifier.dart';
 import 'package:ditonton/common/state_enum.dart';
 import 'package:flutter/material.dart';
@@ -154,16 +153,125 @@ class TvDetailContent extends StatelessWidget {
                           Text(
                             _showGenres(tv.genres),
                           ),
-                          Text(tv.episodeRunTime.toString())
+                          Text(tv.episodeRunTime.toString()),
+                          Row(
+                            children: [
+                              RatingBarIndicator(
+                                rating: tv.voteAverage / 2,
+                                itemCount: 5,
+                                itemBuilder: (context, index) => Icon(
+                                  Icons.star,
+                                  color: kMikadoYellow,
+                                ),
+                                itemSize: 24,
+                              ),
+                              Text('${tv.voteAverage}')
+                            ],
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'Overview',
+                            style: kHeading6,
+                          ),
+                          Text(
+                            tv.overview,
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'Recommendations',
+                            style: kHeading6,
+                          ),
+                          Consumer<TvDetailNotifier>(
+                            builder: (context, data, child) {
+                              if (data.recommendationState ==
+                                  RequestState.Loading) {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              } else if (data.recommendationState ==
+                                  RequestState.Error) {
+                                return Text(data.message);
+                              } else if (data.recommendationState ==
+                                  RequestState.Loaded) {
+                                return Container(
+                                  height: 150,
+                                  child: ListView.builder(
+                                    scrollDirection: Axis.horizontal,
+                                    itemBuilder: (context, index) {
+                                      final tv = recommendations[index];
+                                      return Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: InkWell(
+                                          onTap: () {
+                                            Navigator.pushReplacementNamed(
+                                              context,
+                                              TvDetailPage.ROUTE_NAME,
+                                              arguments: tv.id,
+                                            );
+                                          },
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(8),
+                                            ),
+                                            child: CachedNetworkImage(
+                                              imageUrl:
+                                                  'https://image.tmdb.org/t/p/w500${tv.posterPath}',
+                                              placeholder: (context, url) =>
+                                                  Center(
+                                                child:
+                                                    CircularProgressIndicator(),
+                                              ),
+                                              errorWidget:
+                                                  (context, url, error) =>
+                                                      Icon(Icons.error),
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    itemCount: recommendations.length,
+                                  ),
+                                );
+                              } else {
+                                return Container();
+                              }
+                            },
+                          )
                         ],
                       ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      color: Colors.white,
+                      height: 4,
+                      width: 48,
                     ),
                   )
                 ]),
               );
             },
+            minChildSize: 0.25,
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundColor: kRichBlack,
+            foregroundColor: Colors.white,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back),
+            ),
+          ),
+        )
       ],
     );
   }
