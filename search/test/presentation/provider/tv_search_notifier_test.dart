59d11fb6251c38e5c6529013952f3c50d205be14
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-11-19 15:00:40 +0700

    Perbaikan testing untuk tv search notifier

--- search/test/presentation/provider/tv_search_notifier_test.dart
+++ search/test/presentation/provider/tv_search_notifier_test.dart
@@ -2,25 +2,25 @@ import 'package:dartz/dartz.dart';
 import 'package:core/utils/failure.dart';
 import 'package:core/utils/state_enum.dart';
 import 'package:core/domain/entities/tv/tv.dart';
-import 'package:core/domain/usecases/tv/get_top_rated_tv.dart';
-import 'package:core/presentation/provider/tv/top_rated_tv_notifier.dart';
+import 'package:search/domain/usecases/search_tv.dart';
+import 'package:search/presentation/provider/tv_search_notifier.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'top_rated_tv_notifier_test.mocks.dart';
+import 'tv_search_notifier_test.mocks.dart';
 
-@GenerateMocks([GetTopRatedTv])
+@GenerateMocks([SearchTv])
 void main() {
-  late MockGetTopRatedTv mockGetTopRatedTv;
-  late TopRatedTvNotifier notifier;
+  late TvSearchNotifier provider;
+  late MockSearchTv mockSearchTv;
   late int listenerCallCount;
 
   setUp(
     () {
       listenerCallCount = 0;
-      mockGetTopRatedTv = MockGetTopRatedTv();
-      notifier = TopRatedTvNotifier(getTopRatedTv: mockGetTopRatedTv)
+      mockSearchTv = MockSearchTv();
+      provider = TvSearchNotifier(searchTv: mockSearchTv)
         ..addListener(() {
           listenerCallCount++;
         });
@@ -44,37 +44,42 @@ void main() {
   );
 
   final tTvList = <Tv>[tTv];
+  final tQuery = 'game of thrones';
 
-  test('should change state to loading when usecase is called', () async {
-    // arrange
-    when(mockGetTopRatedTv.execute()).thenAnswer((_) async => Right(tTvList));
-    // act
-    notifier.fetchTopRatedTv();
-    // assert
-    expect(notifier.state, RequestState.loading);
-    expect(listenerCallCount, 1);
-  });
+  group('search Tv', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockSearchTv.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvList));
+      // act
+      provider.fetchTvSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.loading);
+    });
 
-  test('should change Tv data when data is gotten successfully', () async {
-    // arrange
-    when(mockGetTopRatedTv.execute()).thenAnswer((_) async => Right(tTvList));
-    // act
-    await notifier.fetchTopRatedTv();
-    // assert
-    expect(notifier.state, RequestState.loaded);
-    expect(notifier.tv, tTvList);
-    expect(listenerCallCount, 2);
-  });
+    test('should change search result data when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockSearchTv.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvList));
+      // act
+      await provider.fetchTvSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.loaded);
+      expect(provider.searchResult, tTvList);
+      expect(listenerCallCount, 2);
+    });
 
-  test('should return error when data is unsuccessful', () async {
-    // arrange
-    when(mockGetTopRatedTv.execute())
-        .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
-    // act
-    await notifier.fetchTopRatedTv();
-    // assert
-    expect(notifier.state, RequestState.error);
-    expect(notifier.message, 'Server Failure');
-    expect(listenerCallCount, 2);
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockSearchTv.execute(tQuery))
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchTvSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
   });
 }
