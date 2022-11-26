cfbd40e2696ccd4c901fefef48d6cc035166206f
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-11-26 19:19:39 +0700

    Menambahkan test untuk tv season detail bloc dan tv episode detail bloc

---
+++ modules/tv_series/test/presentation/bloc/tv_episode_detail_bloc_test.dart
@@ -0,0 +1,75 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:core/core.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tv_series/tv_series.dart';
+
+import 'tv_episode_detail_bloc_test.mocks.dart';
+import '../../dummy_data/tv_dummy_objects.dart';
+
+@GenerateMocks([GetTvEpisodeDetail])
+void main() {
+  late TvEpisodeDetailBloc tvEpisodeDetailBloc;
+  late MockGetTvEpisodeDetail mockGetTvEpisodeDetail;
+
+  setUp(
+    () {
+      mockGetTvEpisodeDetail = MockGetTvEpisodeDetail();
+      tvEpisodeDetailBloc = TvEpisodeDetailBloc(mockGetTvEpisodeDetail);
+    },
+  );
+
+  test(
+    'initial state should be empty',
+    () {
+      expect(tvEpisodeDetailBloc.state, TvEmpty());
+    },
+  );
+
+  blocTest<TvEpisodeDetailBloc, TvState>(
+    'emits [Loading, HasData] when data is gotten successfully.',
+    build: () {
+      when(mockGetTvEpisodeDetail.execute(
+              testTvDetail.id,
+              testTvSeasonDetail.seasonNumber,
+              testTvEpisodeDetail.episodeNumber))
+          .thenAnswer((realInvocation) async => Right(testTvEpisodeDetail));
+      return tvEpisodeDetailBloc;
+    },
+    act: (bloc) => bloc.add(OnTvEpisodeDetail(testTvDetail.id,
+        testTvSeasonDetail.seasonNumber, testTvEpisodeDetail.episodeNumber)),
+    expect: () => <TvState>[
+      TvLoading(),
+      TvEpisodeDetailHasData(testTvEpisodeDetail),
+    ],
+    verify: (bloc) {
+      verify(mockGetTvEpisodeDetail.execute(testTvDetail.id,
+          testTvSeasonDetail.seasonNumber, testTvEpisodeDetail.episodeNumber));
+    },
+  );
+
+  blocTest<TvEpisodeDetailBloc, TvState>(
+    'emits [Loading, Error] when get tv episode detail in unsuccessful.',
+    build: () {
+      when(mockGetTvEpisodeDetail.execute(
+              testTvDetail.id,
+              testTvSeasonDetail.seasonNumber,
+              testTvEpisodeDetail.episodeNumber))
+          .thenAnswer(
+              (realInvocation) async => Left(ServerFailure('Server Failure')));
+      return tvEpisodeDetailBloc;
+    },
+    act: (bloc) => bloc.add(OnTvEpisodeDetail(testTvDetail.id,
+        testTvSeasonDetail.seasonNumber, testTvEpisodeDetail.episodeNumber)),
+    expect: () => <TvState>[
+      TvLoading(),
+      TvError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockGetTvEpisodeDetail.execute(testTvDetail.id,
+          testTvSeasonDetail.seasonNumber, testTvEpisodeDetail.episodeNumber));
+    },
+  );
+}
---
+++ modules/tv_series/test/presentation/bloc/tv_season_detail_bloc_test.dart
@@ -0,0 +1,71 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:core/core.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tv_series/tv_series.dart';
+
+import 'tv_season_detail_bloc_test.mocks.dart';
+import '../../dummy_data/tv_dummy_objects.dart';
+
+@GenerateMocks([GetTvSeasonDetail])
+void main() {
+  late TvSeasonDetailBloc tvSeasonDetailBloc;
+  late MockGetTvSeasonDetail mockGetTvSeasonDetail;
+
+  setUp(
+    () {
+      mockGetTvSeasonDetail = MockGetTvSeasonDetail();
+      tvSeasonDetailBloc = TvSeasonDetailBloc(mockGetTvSeasonDetail);
+    },
+  );
+
+  test(
+    'inital state should be empty',
+    () {
+      expect(tvSeasonDetailBloc.state, TvEmpty());
+    },
+  );
+
+  blocTest<TvSeasonDetailBloc, TvState>(
+    'emits [Loading, HasData] when data is gotten successfully.',
+    build: () {
+      when(mockGetTvSeasonDetail.execute(
+              testTvDetail.id, testTvSeasonDetail.seasonNumber))
+          .thenAnswer((realInvocation) async => Right(testTvSeasonDetail));
+      return tvSeasonDetailBloc;
+    },
+    act: (bloc) => bloc.add(
+        OnTvSeasonDetail(testTvDetail.id, testTvSeasonDetail.seasonNumber)),
+    expect: () => <TvState>[
+      TvLoading(),
+      TvSeasonDetailHasData(testTvSeasonDetail),
+    ],
+    verify: (bloc) {
+      verify(mockGetTvSeasonDetail.execute(
+          testTvDetail.id, testTvSeasonDetail.seasonNumber));
+    },
+  );
+
+  blocTest<TvSeasonDetailBloc, TvState>(
+    'emits [Loading, Error] when get tv season detail is unsuccessful.',
+    build: () {
+      when(mockGetTvSeasonDetail.execute(
+              testTvDetail.id, testTvSeasonDetail.seasonNumber))
+          .thenAnswer(
+              (realInvocation) async => Left(ServerFailure('Server Failure')));
+      return tvSeasonDetailBloc;
+    },
+    act: (bloc) => bloc.add(
+        OnTvSeasonDetail(testTvDetail.id, testTvSeasonDetail.seasonNumber)),
+    expect: () => <TvState>[
+      TvLoading(),
+      TvError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockGetTvSeasonDetail.execute(
+          testTvDetail.id, testTvSeasonDetail.seasonNumber));
+    },
+  );
+}
